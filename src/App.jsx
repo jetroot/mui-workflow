9e5c6b3467b3e6b00599ea473558e1bbbf17c2e5
hashgit <abdrazakghiyati2@gmail.com>
2022-12-26 15:57:51 -0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Typography } from "@mui/material";
+import FlexBetween from "./components/FlexBetween";
+
+const App = () => {
+    return (
+        <FlexBetween
+            sx={{
+                width: "100vw",
+                height: "100vh",
+                backgroundColor: "#CD6858",
+                display: "flex",
+                justifyContent: "center",
+            }}
+        >
+            <Typography variant="h2" component={"h4"}>
+                Automated with Github Actions
+            </Typography>
+        </FlexBetween>
+    );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Container, CssBaseline } from "@mui/material";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+import { unstable_ClassNameGenerator as ClassNameGenerator } from "@mui/material/className";
+
+ClassNameGenerator.configure(
+    // Do something with the componentName
+    (componentName) => ""
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <CssBaseline />
+        <App />
+    </React.StrictMode>
+);
